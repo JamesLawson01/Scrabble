26bea85bbe1b7889ac33fd0e32b440d808683451
James Lawson <67314546+JamesLawson01@users.noreply.github.com>
2021-12-10 18:45:37 +0000

    Validate word with SOWPODS

--- Scrabble/Word.cs
+++ Scrabble/Word.cs
@@ -1,23 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 
 namespace Scrabble
 {
     public class Word
     {
-        public string word { get; }
+        private const string sowpodsLocation = @"Sowpods.txt";
+
+        private static bool previouslyValidated = false;
+
+        private static HashSet<string> sowpods;
+
+        public List<Tile> word { get; }
 
         public Bonus WordBonus { get; }
 
-        public Word(string word)
+        public Word(List<Tile> word)
         {
             this.word = word;
         }
 
-        public bool Validate(Word word)
+        public bool Validate()
         {
-            //
-            return true;
+            if (!previouslyValidated)
+            {
+                InitSowpods();
+                previouslyValidated = true;
+            }
+            return sowpods.Contains(ToString());
+        }
+
+        private static void InitSowpods()
+        {
+            sowpods = new HashSet<string>();
+            StreamReader sr = new StreamReader(sowpodsLocation);
+            while (sr.Peek() != -1)
+            {
+                sowpods.Add(sr.ReadLine());
+            }
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (Tile tile in word)
+            {
+                str += tile.Letter;
+            }
+            return str.ToLower();
         }
     }
 }
